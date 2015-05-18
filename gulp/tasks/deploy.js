f725c30f76988051181b3fc408ed0bdbf0229a31
Duncan Beaton <dunckr@gmail.com>
2015-05-18 21:50:44 +0100

    Update 2015-05-18T20:50:37.042Z

--- gulp/tasks/deploy.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-var deploy = require('gulp-gh-pages');
-
-gulp.task('deploy', ['production'], function() {
-  return gulp.src('./build/**/*')
-    .pipe(deploy());
-});
--- gulp/tasks/production.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-
-// Run this to compress all the things!
-//gulp.task('production', ['karma'], function(){
-gulp.task('production', function() {
-  // This runs only if the karma tests pass
-  gulp.start(['markup', 'images', 'iconFont', 'minifyCss', 'uglifyJs'])
-});
